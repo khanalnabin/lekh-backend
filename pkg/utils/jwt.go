3b79ebe577800525cfcaf87e479aabf288b7ce90
Nabin Khanal <nabinkhanal@skiff.com>
2023-10-17 22:26:36 +0545

    some utils added

---
+++ pkg/utils/jwt.go
@@ -0,0 +1,67 @@
+package utils
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type Tokens struct {
+	Access  string
+	Refresh string
+}
+
+func GenerateNewTokens(id string) (*Tokens, error) {
+	accessToken, err := generateNewAccessToken(id)
+	if err != nil {
+		return nil, err
+	}
+	refreshToken, err := generateNewRefreshToken()
+	if err != nil {
+		return nil, err
+	}
+	return &Tokens{
+		Access:  accessToken,
+		Refresh: refreshToken,
+	}, nil
+
+}
+
+func generateNewAccessToken(id string) (string, error) {
+	secret := os.Getenv("JWT_SECRET_KEY")
+	minutesCount, _ := strconv.Atoi(os.Getenv("JWT_SECRET_KEY_EXPIRE_MINUTE_COUNT"))
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":      id,
+		"expires": time.Now().Add(time.Minute * time.Duration(minutesCount)).Unix(),
+	})
+	t, err := token.SignedString([]byte(secret))
+	if err != nil {
+		return "", err
+	}
+	return t, nil
+}
+
+func generateNewRefreshToken() (string, error) {
+	hash := sha256.New()
+	refresh := os.Getenv("JWT_REFRESH_KEY") + time.Now().String()
+
+	_, err := hash.Write([]byte(refresh))
+	if err != nil {
+		return "", err
+	}
+	hoursCount, _ := strconv.Atoi(os.Getenv("JWT_REFRESH_KEY_EXPIRE_HOURS_COUNT"))
+	expireTime := fmt.Sprint(time.Now().Add(time.Hour * time.Duration(hoursCount)).Unix())
+	t := hex.EncodeToString(hash.Sum(nil)) + "." + expireTime
+	return t, nil
+}
+
+func ParseRefreshToken(refreshToken string) (int64, error) {
+	return strconv.ParseInt(strings.Split(refreshToken, ".")[1], 0, 64)
+}
