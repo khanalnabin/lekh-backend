d20ae27a3a78b4e8b39eecf9c3577bedde244201
Nabin Khanal <nabinkhanal@skiff.com>
2023-10-17 22:28:02 +0545

    final startup

--- main.go
+++ main.go
@@ -1,22 +1,31 @@
 package main
 
 import (
-	"os"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
+	_ "github.com/joho/godotenv/autoload"
+	"gitlab.com/nabinkhanal/lekh-backend/pkg/configs"
+	"gitlab.com/nabinkhanal/lekh-backend/pkg/middlewares"
+	"gitlab.com/nabinkhanal/lekh-backend/pkg/routes"
+	"gitlab.com/nabinkhanal/lekh-backend/pkg/utils"
+	"gitlab.com/nabinkhanal/lekh-backend/platform/database"
 )
 
-func getPort() string {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = ":3000"
-	} else {
-		port = ":" + port
-	}
-	return port
-}
-
 func main() {
-	app := fiber.New()
-	app.Listen(getPort())
+
+	if err := database.Connect(); err != nil {
+		log.Fatal(err)
+	}
+
+	config := configs.FiberConfig()
+	app := fiber.New(config)
+	middlewares.FiberMiddleware(app)
+	routes.PublicRoutes(app)
+	routes.PrivateRoutes(app)
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello")
+	})
+	utils.Start(app)
 }
