4a91401e00b01c2ea4087756ea8d283b66ef2675
Nabin Khanal <nabinkhanal@skiff.com>
2023-10-18 21:23:15 +0545

    some project configuration changes

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gofiber/fiber/v2 v2.50.0
 	github.com/golang-jwt/jwt/v5 v5.0.0
 	github.com/joho/godotenv v1.5.1
+	github.com/redis/go-redis/v9 v9.2.1
 	go.mongodb.org/mongo-driver v1.12.1
 	golang.org/x/crypto v0.7.0
 )
@@ -14,6 +15,8 @@ require (
 require (
 	github.com/MicahParks/keyfunc/v2 v2.1.0 // indirect
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/uuid v1.3.1 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
--- main.go
+++ main.go
@@ -1,28 +1,22 @@
 package main
 
 import (
-	"log"
-
 	"github.com/gofiber/fiber/v2"
 	_ "github.com/joho/godotenv/autoload"
 	"gitlab.com/nabinkhanal/lekh-backend/pkg/configs"
 	"gitlab.com/nabinkhanal/lekh-backend/pkg/middlewares"
 	"gitlab.com/nabinkhanal/lekh-backend/pkg/routes"
 	"gitlab.com/nabinkhanal/lekh-backend/pkg/utils"
-	"gitlab.com/nabinkhanal/lekh-backend/platform/database"
 )
 
 func main() {
-
-	if err := database.Connect(); err != nil {
-		log.Fatal(err)
-	}
 
 	config := configs.FiberConfig()
 	app := fiber.New(config)
 	middlewares.FiberMiddleware(app)
 	routes.PublicRoutes(app)
 	routes.PrivateRoutes(app)
+	routes.NotFoundRoute(app)
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello")
